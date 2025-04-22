e79bd8ac98a033542d43b22b7d9b861c9c5724e9
Alex Aaron <alexfrancisaaron@gmail.com>
2025-04-22 13:38:59 -0500

    added text to Hoomepage

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -3,6 +3,7 @@ function Homepage(props){
   return (
     <div className="home">
       <h1>Homepage</h1>
+      <h2>Added text</h2>
       <h2>{props.firstName} is {props.age} years old.</h2>
     </div>
   )
